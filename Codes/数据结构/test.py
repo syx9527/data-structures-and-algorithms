35e22219b134584d2838f87da32d26acca4fc274
syx <1275241305@qq.com>
2022-08-09 11:35:55 +0800

    test

---
+++ Codes/数据结构/test.py
@@ -0,0 +1,24 @@
+import time
+
+a = 1
+
+
+def A(l):
+    a = 5
+    b = 3
+
+
+def B():
+    ...
+
+
+def C():
+    pass
+
+
+def mainc():
+    A([1, 2, 3])
+
+
+if __name__ == '__main__':
+    mainc()
